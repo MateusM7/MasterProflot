4bc1d74ae3c84bc7f4a1d7bb9acb078406458784
Mateus Costa <mateus.pc7@hotmail.com>
2017-05-27 11:00:36 -0300

    ProfessorController,TipoController menu

--- resources/views/menu.blade.php
+++ resources/views/menu.blade.php
@@ -5,94 +5,60 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Laravel</title>
-
     <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css" rel="stylesheet">
-
     <!-- Fonts -->
     <link href='//fonts.googleapis.com/css?family=Roboto:400,300' rel='stylesheet' type='text/css'>
 
-    
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
         <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
         <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+        <![endif]-->
         <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
-        <style type="text/css">
+        <link rel="stylesheet" href="{{asset('css/menu.css')}}">
+    </head>
+    <body>
+        <nav class="navbar navbar-default">
+            <div class="container-fluid">
+                <div class="navbar-header">
+                    <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar">
+                        <span class="sr-only">Toggle Navigation</span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                    </button>
+                    <a class="navbar-brand" href="#">[ PROFLOT ]</a>
+                </div>
 
-        body,html{
-            height: 100%;
-        }
+                <div class="collapse navbar-collapse" id="navbar">
+                    <ul class="nav navbar-nav">
 
-        /* remove outer padding */
-        .container-main .row{
-            padding: 0px;
-            margin: 0px;
-        }
+                    </ul>
 
-        /*Remove rounded coners*/
-
-        nav.sidebar.navbar {
-            border-radius: 0px;
-        }
-
-        nav.sidebar, .container-main{
-            -webkit-transition: margin 200ms ease-out;
-            -moz-transition: margin 200ms ease-out;
-            -o-transition: margin 200ms ease-out;
-            transition: margin 200ms ease-out;
-        }
-
-        /* Icons */
-        .menu-icon {
-            font-size: 20px;
-        }
-
-        /* Add gap to nav and right windows.*/
-   container-main{
-    padding: 10px 10px 0 10px;
-    }
-
-    </style>
-
-
-</head>
-<body>
-<nav class="navbar navbar-default">
-        <div class="container-fluid">
-            <div class="navbar-header">
-                <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar">
-                    <span class="sr-only">Toggle Navigation</span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                </button>
-                <a class="navbar-brand" href="#">[ PROFLOT ]</a>
-            </div>
-
-            <div class="collapse navbar-collapse" id="navbar">
-                <ul class="nav navbar-nav">
-                
-                </ul>
-
-                <ul class=" navbar-right">
-                    @if(auth()->guest())
+                    <ul class="nav navbar-nav navbar-right">
+                        @if(auth()->guest())
                         @if(!Request::is('auth/login'))
-                            <li><a href="{{ url('/auth/login') }}">Login</a></li>
+                        <li><a href="{{ url('/auth/login') }}">Login</a></li>
                         @endif
                         @if(!Request::is('auth/register'))
-                            <li><a href="{{ url('/auth/register') }}">Register</a></li>
+                        <li><a href="{{ url('/auth/register') }}">Register</a></li>
                         @endif
-                    @else
+                        @else
                         <li class="dropdown">
                             <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">{{ auth()->user()->name }} <span class="caret"></span></a>
                             <ul class="dropdown-menu" role="menu">
                                 <li><a href="{{ url('/auth/logout') }}">Logout</a></li>
                             </ul>
                         </li>
-                    @endif
-                </ul>
+                        @endif
+                    </ul>
+                </div>
             </div>
-        </div>
-    <!-- Collect the nav links, forms, and other content for toggling -->
-            <div class="collapse navbar-collapse" id="bs-sidebar-navbar-collapse-1">
+        </nav>
+        <nav class="navbar navbar-m2p sidebar" role="navigation">
+            <div class="container-fluid">
+             <div class="collapse navbar-collapse" id="bs-sidebar-navbar-collapse-1">
                 <ul class="nav navbar-nav">
                     <!-- Dashboard -->
                     <li class="active open">
@@ -166,7 +132,6 @@
             </ul>
         </div>
     </div>
-     </div>
 </nav>
 
 
